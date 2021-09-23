50f95e735700096d39a518ad64e896baa0e64fca
assopri <i4G4fyaa>
2021-09-23 08:49:27 +0300

    version to fork

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -26,7 +26,7 @@ namespace TestConsoleApp
             parameters.Add("cancellation_token", CancellationToken.None);
             
             CefBrowserWrapperFactoryBase factory = new UniCefBrowserWrapperFactory(true);
-            CefBrowserWrapperBase cefBrowserWrapper = factory.Create(true, true, 10000, false, new SingleBrowserInfo("", ""));
+            CefBrowserWrapperBase cefBrowserWrapper = factory.Create(true, true, 10000, false, new SingleBrowserInfo("", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"));
             parameters.Add("cef_browser_wrapper", cefBrowserWrapper);
 
             try
